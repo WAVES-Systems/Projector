89eeb5292e25dc45a08ef327577ba591caff6899
Kennedy Edson Silva de Souza (SIDI) <k.souza@dmz.org.br>
2022-12-13 12:19:48 -0300

    main: Add assemblyInfo

--- Sample/Properties/AssemblyInfo.cs
+++ Sample/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sample")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WavesSystems.Projector")]
+[assembly: AssemblyDescription("UI Element that reproduces animations based on sprite sheet files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sample")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("WAVES Systems")]
+[assembly: AssemblyProduct("WavesSystems.Projector")]
+[assembly: AssemblyCopyright("Copyright © 2022 WAVES Systems")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.1")]
+[assembly: AssemblyFileVersion("1.0.3.1")]
+[assembly: NeutralResourcesLanguage("en-US")]

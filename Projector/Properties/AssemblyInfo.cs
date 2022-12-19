de7c302a253d669b5d207cbaedad067f6a4ba80b
Kennedy Souza <kennedy.es.souza@outlook.com>
2022-12-19 14:18:55 -0300

    dev: update assembly title.

--- Projector/Properties/AssemblyInfo.cs
+++ Projector/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Waves.Visual.Projector")]
+[assembly: AssemblyTitle("WavesSystems.Projector")]
 [assembly: AssemblyDescription("UI Element that reproduces animations based on sprite sheet files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WAVES Systems")]
